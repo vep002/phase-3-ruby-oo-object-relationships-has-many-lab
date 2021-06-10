76157a6fbc7e6041b8145023039b7b1519bd0595
Victoria <vep002@gmail.com>
2021-06-10 08:28:22 -0500

    commit tree c26c402d9c57ed70e4be924faf40f5dcaf6eca11

---
+++ lib/author.rb
@@ -0,0 +1,22 @@
+class Author
+    attr_accessor :name
+    def initialize(name)
+        @name=name
+    end
+
+    def add_post(post)
+        post.author=self
+    end
+
+    def posts
+        Post.all.select {|post| post.author==self}
+    end
+
+    def add_post_by_title(post)
+        post=Post.new(post)
+        post.author=self
+    end
+
+
+
+end
